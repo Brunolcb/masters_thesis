fix dice averaging

src/train.py
@@ -112,10 +112,10 @@ def data_pass(model, loader, loss_fn, optimizer=None, scaler=None,
         # metrics
         train_preds = torch.sigmoid(predictions)
         train_preds = (train_preds > 0.5).float()
-        epoch_dice += dice_coef(train_preds, targets).item() * len(targets)
+        epoch_dice += dice_coef(train_preds, targets).item()
         epoch_loss += loss.item() * len(targets)
     epoch_loss = epoch_loss / len(loader.dataset)
-    epoch_dice = epoch_dice / len(loader.dataset)
+    epoch_dice = epoch_dice / len(loader)
 
     return epoch_loss, epoch_dice
 