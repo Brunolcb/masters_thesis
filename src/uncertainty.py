189fecede8c6e5ec4c666f8f970f0138a8e03194
Brunolcb <brunolaboissiere@hotmail.com>
2023-07-04 14:49:52 -0300

    Update with SoftDice and BCE loss as uncertainty

--- src/uncertainty.py
+++ src/uncertainty.py
@@ -176,7 +176,69 @@ class DSCIntegralOverThreshold(SegmentationUncertainty):
             dscs += dice_coef(p > threshold, pred)
 
         return -dscs
+class DSCIntegralOverThreshold(SegmentationUncertainty):
+    def __init__(self, threshold_lims=(.05, .95), step=.05):
+        super().__init__()
+        self.threshold_lims = threshold_lims
+        self.step = step
+    
+    def metric(self, probs: np.array) -> float:
+        """
+        :param probs: array [num_classes, *image_shape]
+        :return: float
+        """
+        p = np.sum(probs[1:], axis=0)  # probability of foreground
+        pred = p > .5
 
+        dscs = 0
+        for threshold in np.arange(self.threshold_lims[0], self.threshold_lims[1] + self.step, self.step):
+            dscs += dice_coef(p > threshold, pred)
+
+        return -dscs
+
+
+class BinaryCrossEntropy(SegmentationUncertainty):
+    def __init__(self, threshold_lim=.5):
+        super().__init__()
+        self.threshold_lim = threshold_lim
+    def metric(self, probs: np.array) -> float:
+        p = np.sum(probs[1:], axis=0) 
+        y_true = p > self.threshold_lim
+        y_pred = np.clip(p, 1e-7, 1 - 1e-7)
+        term_0 = (1-y_true) * np.log(1-y_pred + 1e-7)
+        term_1 = y_true * np.log(y_pred + 1e-7)
+        
+        return -np.mean(term_0+term_1)
+
+class FocalLoss(SegmentationUncertainty):
+    def __init__(self, threshold_lim=.5, gamma=2):
+        super().__init__()
+        self.threshold_lim = threshold_lim
+        self.gamma =gamma
+    def metric(self, probs: np.array) -> float:
+        y_pred = np.sum(probs[1:], axis=0) 
+        y_true = y_pred > self.threshold_lim
+        eps = 1e-12
+        # If actual value is true, keep pt value as y_pred otherwise (1-y_pred)
+        pt = np.where(y_true == 1, y_pred, 1-y_pred)
+        # Clip values below epsilon and above 1-epsilon
+        pt = np.clip(pt, eps, 1-eps)
+        # FL = -(1-pt)^gamma log(pt)
+        focal_loss = -np.mean(np.multiply(np.power(1-pt,self.gamma),np.log(pt)))    
+        return focal_loss   
+    
+class SoftDice(SegmentationUncertainty):
+    def __init__(self, threshold_lim=.5):
+        super().__init__()
+        self.threshold_lim = threshold_lim
+    def metric(self, probs: np.array) -> float:
+        y_pred = np.sum(probs[1:], axis=0) 
+        y_true = y_pred > self.threshold_lim
+        eps = 1e-12
+        intersection = np.sum(y_pred*y_true)                          
+        dice = (2.*intersection + eps)/(np.sum(y_pred) + np.sum(y_true) + eps)  
+        return 1-dice   
+    
 #funções de medidas de incertezas
 def entropy_of_expected(probs, epsilon=1e-10):
     """
