ab3a9dc6b0d72793892ac9850914d419a2a65654
Brunolcb <brunolaboissiere@hotmail.com>
2023-09-14 20:50:06 -0300

    Add HD95 metric and plot it

--- src/utils.py
+++ src/utils.py
@@ -6,14 +6,17 @@ from sklearn.metrics import auc
 from src.metrics import rc_curve
 
 
-def plot_rc_curves(uncertainties: dict, scores):
+def plot_rc_curves(uncertainties: dict, scores, type_dice=True):
     for metric, uncertainty in uncertainties.items():
-        coverages, risks, _ = rc_curve(-uncertainty, scores, expert=False)
+        coverages, risks, _ = rc_curve(-uncertainty, scores, expert=False, expert_cost=0, type_dice=type_dice)
         rcauc = auc(coverages, risks)
         plt.plot(coverages, risks, label=f"{metric} ({rcauc:.3f})")
 
     #ideal RC curve
-    coverages, risks, _ = rc_curve(scores, scores, expert=False)
+    if type_dice == True:
+        coverages, risks, _ = rc_curve(scores, scores, expert=False, expert_cost=0)
+    else:
+        coverages, risks, _ = rc_curve(-scores, scores, expert=False, expert_cost=0, type_dice=type_dice)
     rcauc = auc(coverages, risks)
     plt.plot(coverages, risks, linestyle='--', label=f"ideal ({rcauc:.3f})")
 
@@ -22,7 +25,7 @@ def plot_rc_curves(uncertainties: dict, scores):
     n_random = 10
     for _ in range(n_random):
         coverages, risks, _ = rc_curve(np.random.rand(*scores.shape),
-                                       scores, expert=False)
+                                       scores, expert=False, expert_cost=0, type_dice=type_dice)
         random_risks += risks / n_random
     rcauc = auc(coverages, random_risks)
     plt.plot(coverages, random_risks, linestyle='--', label=f"random ({rcauc:.3f})")
